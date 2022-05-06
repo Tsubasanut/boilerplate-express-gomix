6f8bee713f78792685a02a0c8a6bd849d736abdd
tsubasanut <tsubasanut@gmail.com>
2022-05-06 10:45:18 +0300

    2nd

--- myApp.js
+++ myApp.js
@@ -1,6 +1,11 @@
 let express = require("express");
 let app = express();
 
-console.log("Hello world");
+app.get("/", returnHello);
+
+function returnHello(req, res) {
+  //console.log(`running respose ${res} for request ${req}`)
+  res.send("Hello Express");
+}
 
 module.exports = app;
