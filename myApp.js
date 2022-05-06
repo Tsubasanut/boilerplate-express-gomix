649bb8b7cf7932bff3a44efbd86c219892825acb
tsubasanut <tsubasanut@gmail.com>
2022-05-06 10:32:02 +0300

    1st

--- myApp.js
+++ myApp.js
@@ -1,39 +1,6 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+console.log("Hello world");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
