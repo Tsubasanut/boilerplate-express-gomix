91360f3e497f980f5f1dfb33d6c1564538ef9ec4
tsubasanut <tsubasanut@gmail.com>
2022-05-06 13:47:25 +0300

    6th

--- myApp.js
+++ myApp.js
@@ -1,8 +1,13 @@
 let express = require("express");
 let app = express();
 
+//let's connect cuurent env via dotenv module
+require("dotenv").config();
+
+//serving static files
 app.use("/public", express.static(__dirname + "/public"));
 
+//main
 app.get("/", returnSomething);
 app.get("/json", returnJson);
 
@@ -10,12 +15,17 @@ function returnSomething(req, res) {
   //console.log(`running respose ${res} for request ${req}`)
   //res.send("Hello Express");
   //console.log(`sending file from ${__dirname}`);
+  //console.log("test process.env", process.env);
   res.sendFile(__dirname + "/views/index.html");
 }
 
 function returnJson(req, res) {
+  let msgTxt = (process.env.MESSAGE_STYLE = "uppercase"
+    ? "Hello json".toUpperCase()
+    : "Hello json");
+
   const testObj = {
-    message: "Hello json",
+    message: msgTxt,
   };
   res.json(testObj);
 }
