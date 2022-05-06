0488d87f695319d67bfd6985b17d3c498c1fcce9
tsubasanut <tsubasanut@gmail.com>
2022-05-06 11:58:58 +0300

    4th

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", returnSomething);
 
