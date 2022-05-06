7834f9890dfb7a164e03bea5a51b8151a2dcb88b
tsubasanut <tsubasanut@gmail.com>
2022-05-06 11:42:50 +0300

    4th

--- myApp.js
+++ myApp.js
@@ -1,12 +1,14 @@
 let express = require("express");
 let app = express();
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", returnSomething);
 
 function returnSomething(req, res) {
   //console.log(`running respose ${res} for request ${req}`)
   //res.send("Hello Express");
-  console.log(`sending file from ${__dirname}`);
+  //console.log(`sending file from ${__dirname}`);
   res.sendFile(__dirname + "/views/index.html");
 }
 
