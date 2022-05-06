bc074167d87f04f5f14376ba9d136d3187f5e0ae
tsubasanut <tsubasanut@gmail.com>
2022-05-06 12:04:37 +0300

    5th

--- myApp.js
+++ myApp.js
@@ -4,12 +4,20 @@ let app = express();
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", returnSomething);
+app.get("/json", returnJson);
 
 function returnSomething(req, res) {
   //console.log(`running respose ${res} for request ${req}`)
   //res.send("Hello Express");
   //console.log(`sending file from ${__dirname}`);
   res.sendFile(__dirname + "/views/index.html");
+}
+
+function returnJson(req, res) {
+  const testObj = {
+    message: "Hello json",
+  };
+  res.json(testObj);
 }
 
 module.exports = app;
